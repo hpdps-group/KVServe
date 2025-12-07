94edc11f47afa4371ad8c185e2144c764dd03e58
Zedong-Liu <2250594632@qq.com>
2025-12-07 18:41:28 +0800

    Add __init__.py files for compression component directories

---
+++ kvserve/transform/__init__.py
@@ -0,0 +1,9 @@
+"""
+Transform components for KV cache compression
+"""
+
+# Placeholder for future transform implementations
+# Components should inherit from kvserve.manager.components.Transform
+
+__all__ = []
+
